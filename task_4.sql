342a3a50be44955e2c894fe4cc9aba0cc0341251
macro-dynamic <147334988+macro-dynamic@users.noreply.github.com>
2024-10-27 23:56:45 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,15 @@
+-- Use the database passed as an argument
+USE alx_book_store;
 
+-- Query to get the full description of the books table
+SELECT 
+    COLUMN_NAME AS 'Column Name', 
+    COLUMN_TYPE AS 'Column Type', 
+    IS_NULLABLE AS 'Is Nullable', 
+    COLUMN_DEFAULT AS 'Default Value', 
+    EXTRA AS 'Extra Info'
+FROM 
+    information_schema.COLUMNS 
+WHERE 
+    TABLE_NAME = 'Books' 
+    AND TABLE_SCHEMA = 'alx_book_store';
