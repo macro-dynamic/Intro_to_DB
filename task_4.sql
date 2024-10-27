0ddbc3a41fc0adadf8417eaa857894751f2226a4
macro-dynamic <147334988+macro-dynamic@users.noreply.github.com>
2024-10-27 23:57:54 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -9,7 +9,7 @@ SELECT
     COLUMN_DEFAULT AS 'Default Value', 
     EXTRA AS 'Extra Info'
 FROM 
-    information_schema.COLUMNS 
+    INFORMATION_SCHEMA.COLUMNS 
 WHERE 
     TABLE_NAME = 'Books' 
     AND TABLE_SCHEMA = 'alx_book_store';
